6042ea3269ff43ae1c36d504bac89272e271f0bb
barathrajd <barathdeva757@gmail.com>
2023-07-06 21:44:36 +0530

    vue added

---
+++ section-2/app.js
@@ -0,0 +1,14 @@
+/* const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+function addGoal() {
+  const enteredValue = inputEl.value;
+  const listElement = document.createElement('li');
+  listElement.textContent = enteredValue;
+  listEl.appendChild(listElement);
+  inputEl.value = '';
+}
+
+buttonEl.addEventListener('click', addGoal);
+ */
