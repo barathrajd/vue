8682b902d5add615d0bc1b61eaed615d918413b8
barathrajd <barathdeva757@gmail.com>
2023-07-08 23:23:50 +0530

    v-if v-else-if v-else

---
+++ section-6/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredGoal: '',
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoal);
+      this.enteredGoal = '';
+    },
+  },
+});
+
+app.mount('#user-goals');
