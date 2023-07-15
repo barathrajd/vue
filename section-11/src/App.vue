fec456b24703d32b52ad6ca1f757d60952e6e37e
barathrajd <barathdeva757@gmail.com>
2023-07-15 18:02:50 +0530

    vue component added

---
+++ section-11/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>My Friends</h2>
+  <friend-contact></friend-contact>
+  <friend-contact></friend-contact>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+  }
+}
+</script>
---
+++ section-11/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 1,
+        name: 'John',
+        phone: '0123 4567 890',
+        email: 'john@gmail.com'
+      }
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    }
+  }
+}
+</script>
